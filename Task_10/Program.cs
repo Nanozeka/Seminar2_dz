b3de7e6f82208faa1971dd705f4dd2da79c894cd
Evgeniy <nanozeka@gmail.com>
2022-07-19 00:04:36 +0300

    add Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+ Console.WriteLine("Введите трехзначное число ");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+ int res = FindSekond(num);
+
+ Console.WriteLine($" {num} -> {res}");
+
+ int FindSekond(int num)
+
+ {
+ int a = num/10;
+ int res = a %10;
+ return a % 10;
+ }
+
